7cc980e5d009e4253b91fc765cc2d25ba5119aa7
kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-12-02 15:06:49 -0500

    u

--- x.d
+++ x.d
@@ -22,30 +22,22 @@ void split_test(in string str, in string regex_)
 
 void main()
 {
-//--  splitter_test(", abc, de,  fg, hi, ", ", *");
-//--  splitter_test("Das Endspiel # The Final", r"[^#]+\s#"); 
-
-  // matchAll() seems to works like preg_match()
-
-  write(`The output of matchAll("Das Endspiel # The Final", `, `regex("([^#]+)\s#\s(.*)$")`, " is:  ");
-
   auto x = matchAll("Das Endspiel # The Final, ", r"([^#]+)\s#\s(.*)$"); 
-  writeln(x);
 
-  writeln("--------------------\n");
+  writeln(`matchAll("Das Endspiel # The Final", `, `regex("([^#]+)\s#\s(.*)$")`, " = ", x);
 
  /*
    Or you can use splitter() with a sinlge unique occurance of a regex string like "\s#\s", but
    if it contains other occurances of the "\s#\s" regex, this will be also split out from the string.
    Is splitFirst() the solution? 
 
-   TODO:
+TODO:
+
    Examine the splitter and split examples at https://dlang.org/phobos/std_regex.html
+   What is an exmaple of how split and splitter differ, and when you'd prefer split to splitter? 
   */
  
   splitter_test("Das Endspiel # The Final", r"\s#\s"); 
-
   splitter_test("Das Endspiel # The Final # remainder of line", r"\s#\s"); 
-
   split_test("Das Endspiel # The Final # remainder of string", r"\s#\s"); 
 }
